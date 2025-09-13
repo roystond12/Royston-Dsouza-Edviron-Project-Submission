c8eb4580503c7ab84f2cb00d0000f0f5745bd21a
roystond12 <165377079+roystond12@users.noreply.github.com>
2025-09-13 22:57:23 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-// Automatically include token if exists
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
